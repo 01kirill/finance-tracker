49f96c612d757479d1b3637dd408dc504867b66a
01kirill <kirillzinovenko04@gmail.com>
2026-01-21 23:03:26 +0300

    configured endpoints and added to config

---
+++ backend/finance/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .views import WalletViewSet, CategoryViewSet, TransactionViewSet
+
+router = DefaultRouter()
+router.register(r'wallets', WalletViewSet, basename='wallet')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'transactions', TransactionViewSet, basename='transaction')
+
+urlpatterns = router.urls
