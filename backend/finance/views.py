f388ad599822d17f429d8f4368adb8e65dd4470f
01kirill <kirillzinovenko04@gmail.com>
2026-01-21 23:01:08 +0300

    implemented viewsets for models

--- backend/finance/views.py
+++ backend/finance/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Wallet, Category, Transaction
+from .serializers import WalletSerializer, CategorySerializer, TransactionSerializer
 
-# Create your views here.
+class WalletViewSet(viewsets.ModelViewSet):
+    serializer_class = WalletSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Wallet.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(wallet__user=self.request.user)
