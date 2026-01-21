9d8eb0eab1a6facbd3378cae2d3d8fd1b7604bbd
01kirill <kirillzinovenko04@gmail.com>
2026-01-21 22:59:37 +0300

    implemented serializers for models

---
+++ backend/finance/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Wallet, Category, Transaction
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wallet
+        fields = ['id', 'name', 'currency', 'balance', 'created_at']
+        read_only_fields = ['balance']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'transaction_type']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['id', 'wallet', 'category', 'amount', 'date', 'description', 'created_at']
+
+    def validate(self, data):
+        user = self.context['request'].user
+
+        if data['wallet'].user != user:
+            raise serializers.ValidationError("Вы не можете использовать чужой кошелек!")
+
+        if data['category'] and data['category'].user != user:
+            raise serializers.ValidationError("Вы не можете использовать чужую категорию!")
+
+        return data
