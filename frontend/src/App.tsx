bc5a3a2fbddbdbb6798588aa48f643561a67877a
01kirill <kirillzinovenko04@gmail.com>
2026-01-26 19:11:31 +0300

    added Dashboard route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Activate } from './pages/Activate';
+import { Dashboard } from './pages/Dashboard';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/activate/:uid/:token" element={<Activate />} />
-        <Route path="/dashboard" element={<div>Тут будет Дашборд</div>} />
+        <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
